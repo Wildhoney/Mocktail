5a0bf51352998a1531e94a213fd30fc64a224eca
Wildhoney <adam.timberlake@gmail.com>
2015-08-06 11:35:43 +0100

    Reverted tests

--- tests/Basic.test.js
+++ tests/Basic.test.js
@@ -1,6 +1,22 @@
-import {resolve} from '../component/mocktail';
+import {resolve, reset, env, ENV} from '../component/mocktail';
+import bootstrap                  from './test-utils/Bootstrap';
+import Authentication             from './test-utils/modules/Authentication';
+import AuthenticationMock         from './test-utils/modules/AuthenticationMock';
 
-class Authentication {}
-class AuthenticationMock {}
+afterEach(() => reset());
 
-export default resolve(Authentication, AuthenticationMock);
+describe('Mocktail: Basic', () => {
+
+    it('Should be able to resolve the module depending on the environment;', () => {
+
+        env(ENV.PRODUCTION);
+        const FirstModule = resolve(Authentication, AuthenticationMock);
+        expect(FirstModule.name).toEqual('Authentication');
+
+        env(ENV.TESTING);
+        const SecondModule = resolve(Authentication, AuthenticationMock);
+        expect(SecondModule.name).toEqual('AuthenticationMock');
+
+    });
+
+});
